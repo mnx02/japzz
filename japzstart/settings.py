9cb228100427f8f2806d50421aa2d4fd006f88f1
mnx02 <67163094+mnx02@users.noreply.github.com>
2022-01-26 14:22:27 +0530

    commit

--- japzstart/settings.py
+++ japzstart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d9k&j$^hjwngs+8b8f8g^nkj*g^^i60_3j4l6!4p$*oh^5o7+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 # Application definition
 
